0da4e08fb5378bc7acba107d843b1ad623ffa3a1
jjmin321 <jjmin321@naver.com>
2020-03-19 17:27:03 +0900

    All Update

---
+++ api/api.go
@@ -0,0 +1,81 @@
+package api
+
+import (
+	"encoding/json"
+	"fmt"
+	"net/http"
+	"strconv"
+	"sync"
+)
+
+type Result struct {
+	Count      int         `json:"count"`
+	Page       string      `json:"page"`
+	StoreInfos []StoreInfo `json:"storeInfos"`
+	Sales      []Sales     `json:"sales"`
+}
+
+type StoreInfo struct {
+	Code string `json:"code"`
+	Addr string `json:"addr"`
+	Name string `json:"name"`
+}
+
+type Sales struct {
+	Code       string `json:"code"`
+	CreatedAt  string `json:"created_at"`
+	RemainStat string `json:"remain_stat"`
+	StockAt    string `json:"stock_at"`
+}
+
+// 약국 이름을 받아서 약국 코드번호 출력
+func Drugstore(name string, page int, wg *sync.WaitGroup, ch chan string) {
+	//GET 호출
+	stringpage := strconv.Itoa(page)
+	address := "https://8oi9s0nnth.apigw.ntruss.com/corona19-masks/v1/stores/json?page=" + stringpage
+	// fmt.Println(address)
+	resp, err := http.Get(address)
+	if err != nil {
+		panic(err)
+	}
+	defer resp.Body.Close()
+	var result Result
+	dec := json.NewDecoder(resp.Body)
+	dec.Decode(&result)
+
+	for _, v := range result.StoreInfos {
+		if v.Name == name {
+			// for i := 1; i <= 51; i++ {
+			// 	go masks(v.Code, v.Addr, v.Name, i, wg)
+			// }
+			jsonBytes, _ := json.Marshal(v)
+			jsonString := string(jsonBytes)
+			ch <- jsonString
+		}
+	}
+	wg.Done()
+}
+
+func Masks(code string, page int, wg *sync.WaitGroup, ch chan string) {
+	stringpage := strconv.Itoa(page)
+	address := "https://8oi9s0nnth.apigw.ntruss.com/corona19-masks/v1/sales/json?page=" + stringpage
+	// fmt.Println(address)
+	resp, err := http.Get(address)
+	if err != nil {
+		panic(err)
+	}
+	defer resp.Body.Close()
+	var result Result
+	dec := json.NewDecoder(resp.Body)
+	dec.Decode(&result)
+
+	for _, v := range result.Sales {
+		if v.Code == code {
+			fmt.Println(v.Code, v.CreatedAt, v.RemainStat, v.StockAt)
+			jsonBytes, _ := json.Marshal(v)
+			jsonString := string(jsonBytes)
+			ch <- jsonString
+		}
+	}
+	wg.Done()
+}
---
+++ server.go
@@ -0,0 +1,51 @@
+package main
+
+import (
+	"MyMusicAlbum/api"
+	"encoding/json"
+	"net/http"
+	"sync"
+
+	_ "github.com/go-sql-driver/mysql"
+	_ "github.com/jinzhu/gorm"
+	"github.com/labstack/echo"
+	"github.com/labstack/echo/middleware"
+	// jwt "github.com/dgrijalva/jwt-go"
+)
+
+func main() {
+	e := echo.New()
+	e.Use(middleware.LoggerWithConfig(middleware.LoggerConfig{
+		Format: `Time = ${time_rfc3339}], Status = ${status}, Method = ${method}, Uri = ${uri}, User_agent = ${user_agent}, Error = ${error},  Remote_ip = ${remote_ip}` + "\n",
+	}))
+	e.GET("/drugstore", func(c echo.Context) error {
+		name := c.QueryParam("name")
+		slice1 := []string{}
+		ch := make(chan string, 50)
+		var wg sync.WaitGroup
+		for i := 1; i <= 54; i++ {
+			wg.Add(1)
+			go api.Drugstore(name, i, &wg, ch)
+		}
+		wg.Wait()
+		close(ch)
+		for i := range ch {
+			slice1 = append(slice1, i)
+		}
+		jsonBytes, _ := json.Marshal(slice1)
+		return c.JSONBlob(http.StatusOK, jsonBytes)
+	})
+
+	e.GET("/masks", func(c echo.Context) error {
+		code := c.QueryParam("code")
+		ch := make(chan string, 1)
+		var wg sync.WaitGroup
+		for i := 1; i <= 51; i++ {
+			wg.Add(1)
+			go api.Masks(code, i, &wg, ch)
+		}
+		wg.Wait()
+		return c.JSON(http.StatusOK, <-ch)
+	})
+	e.Logger.Fatal(e.Start(":3000"))
+}
