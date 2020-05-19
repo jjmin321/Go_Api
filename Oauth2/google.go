42c87340b66759b0c01d7df33e47dd3ff23c4906
jjmin321 <jjmin321@naver.com>
2020-05-19 21:55:30 +0900

    [ADD] Oauth2 API

---
+++ Oauth2/google.go
@@ -0,0 +1,73 @@
+package oauth2
+
+import (
+	"fmt"
+	"io/ioutil"
+	"net/http"
+	"os"
+
+	"github.com/joho/godotenv"
+	"golang.org/x/oauth2"
+	"golang.org/x/oauth2/google"
+)
+
+var err = godotenv.Load("google.env")
+
+var (
+	googleOauthConfig = &oauth2.Config{
+		RedirectURL:  "http://localhost:3000/callback",
+		ClientID:     os.Getenv("GOOGLE_CLIENT_ID"),
+		ClientSecret: os.Getenv("GOOGLE_SECRET_KEY"),
+		Scopes:       []string{"https://www.googleapis.com/auth/userinfo.email"},
+		Endpoint:     google.Endpoint,
+	}
+	randomState = "random"
+)
+
+func handleHome(w http.ResponseWriter, r *http.Request) {
+	var html = `<html><body><a href="/login">Google Login</a></body></html>`
+	fmt.Fprint(w, html)
+}
+
+func handleLogin(w http.ResponseWriter, r *http.Request) {
+	url := googleOauthConfig.AuthCodeURL(randomState)
+	http.Redirect(w, r, url, http.StatusTemporaryRedirect)
+}
+
+func handleCallback(w http.ResponseWriter, r *http.Request) {
+	if r.FormValue("state") != randomState {
+		fmt.Println("state is not valid")
+		http.Redirect(w, r, "/", http.StatusTemporaryRedirect)
+		return
+	}
+	token, err := googleOauthConfig.Exchange(oauth2.NoContext, r.FormValue("code"))
+	if err != nil {
+		fmt.Printf("could not get token: %s\n", err.Error())
+		http.Redirect(w, r, "/", http.StatusTemporaryRedirect)
+		return
+	}
+	resp, err := http.Get("https://www.googleapis.com/oauth2/v2/userinfo?access_token=" + token.AccessToken)
+	if err != nil {
+		fmt.Printf("could not create get request: %s\n", err.Error())
+		http.Redirect(w, r, "/", http.StatusTemporaryRedirect)
+		return
+	}
+
+	defer resp.Body.Close()
+	content, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		fmt.Printf("could not parse response: %s\n", err.Error())
+		http.Redirect(w, r, "/", http.StatusTemporaryRedirect)
+		return
+	}
+
+	fmt.Fprintf(w, "Response: %s", content)
+}
+
+//////////////////////////////////////// API 사용 예시 ////////////////////////////////////////
+// func main() {
+// 	http.HandleFunc("/", handleHome)
+// 	http.HandleFunc("/login", handleLogin)
+// 	http.HandleFunc("/callback", handleCallback)
+// 	http.ListenAndServe(":3000", nil)
+// }
